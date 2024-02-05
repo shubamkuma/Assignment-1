701bef5430b34b12b369cb48b3b2d14b5d81d39f
shubamkuma <shubamkumaraug@gmail.com>
2024-02-05 17:48:11 -0500

    Pet care action

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -1,28 +1,58 @@
-﻿using System;
+﻿
+using System;
 
-namespace VirtualPet
+class Pet
 {
-    class Program
+    public string Name { get; set; }
+    public int Hunger { get; set; }
+    public int Happiness { get; set; }
+    public int Health { get; set; }
+
+    public Pet(string name)
     {
-        public static string Cat { get; private set; }
+        Name = name;
+        Hunger = 50;
+        Happiness = 50;
+        Health = 50;
+    }
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Virtual Pet!");
+    public void Feed()
+    {
+        Hunger -= 20;
+        if (Hunger < 0) Hunger = 0;
+        Health += 10;
+        Console.WriteLine("You fed " + Name + ". Hunger decreased, and health increased.");
+    }
 
-            // Pet Creation
-            Console.WriteLine("Choose a pet type (cat, dog, rabbit): ");
-            string petType = Console.ReadLine();
-            Console.WriteLine("Enter a name for your pet: ");
-            string Chini = Console.ReadLine();
+    public void Play()
+    {
+        Happiness += 20;
+        if (Happiness > 100) Happiness = 100;
+        Hunger += 10;
+        if (Hunger > 100) Hunger = 100;
+        Console.WriteLine("You played with " + Name + ". Happiness increased, but hunger also increased.");
+    }
 
-            Pet myPet = new Pet(Cat, Chini);
+    public void Rest()
+    {
+        Health += 20;
+        if (Health > 100) Health = 100;
+        Happiness -= 10;
+        if (Happiness < 0) Happiness = 0;
+        Console.WriteLine("You let " + Name + " rest. Health improved, but happiness decreased slightly.");
+    }
+}
 
-            // Display pet information
-            Console.WriteLine($"Congratulations! You have a new {myPet.Cat} named {myPet.Chini}.");
+class Program
+{
+    static void Main()
+    {
+        Pet pet = new Pet("Fido");
+        Console.WriteLine("Welcome to the virtual pet simulator! Your pet's name is " + pet.Name);
 
-            // Pet Care Loop
-            bool exit = false;
-        }
+        // Example of caring for the pet
+        pet.Feed();
+        pet.Play();
+        pet.Rest();
     }
 }
