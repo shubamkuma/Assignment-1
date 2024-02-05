0919ca8c8e46023ec2814dd1529cb5d65dfe57b2
shuba <shuba@SHUBAM1>
2024-02-05 15:08:47 -0500

    Add project files.

---
+++ Assignment 1/Pet.cs
@@ -0,0 +1,17 @@
+﻿namespace VirtualPet
+{
+    internal class Pet
+    {
+        private string cat;
+        private string? chini;
+
+        public Pet(string cat, string? chini)
+        {
+            this.cat = cat;
+            this.chini = chini;
+        }
+
+        public object Cat { get; internal set; }
+        public object Chini { get; internal set; }
+    }
+}
---
+++ Assignment 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        public static string Cat { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Virtual Pet!");
+
+            // Pet Creation
+            Console.WriteLine("Choose a pet type (cat, dog, rabbit): ");
+            string petType = Console.ReadLine();
+            Console.WriteLine("Enter a name for your pet: ");
+            string Chini = Console.ReadLine();
+
+            Pet myPet = new Pet(Cat, Chini);
+
+            // Display pet information
+            Console.WriteLine($"Congratulations! You have a new {myPet.Cat} named {myPet.Chini}.");
+
+            // Pet Care Loop
+            bool exit = false;
+        }
+    }
+}
